f6591782a9ca50c03f0a335828c6c336db39a7d4
noz-23 <nzds23@yahoo.co.jp>
2026-01-05 16:57:24 +0900

    Make MainWindow

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # AdobePDFServicesFront
 Adobe PDF Services Front End
+
+
+# —˜—pƒ‰ƒCƒuƒ‰ƒŠ
+
+## Adobe.PDFServicesSDK
+https://developer.adobe.com/document-services/docs/overview/pdf-services-api/
+
+https://www.nuget.org/packages/Adobe.PDFServicesSDK/4.3.1/license
+
+## Microsoft.Web.WebView2
+https://learn.microsoft.com/ja-jp/microsoft-edge/webview2/
+
+https://www.nuget.org/packages/Microsoft.Web.WebView2/1.0.3650.58/license
---
+++ Windows/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace AdobePDFServicesFront.Windows;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window,INotifyPropertyChanged
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        //
+        DataContext = this;
+        if (Directory.Exists(Resource.TempPath) == false)
+        {
+            Directory.CreateDirectory(Resource.TempPath);
+        }
+    }
+
+    private async void _loaded(object sender, RoutedEventArgs e)
+    {
+        await _webView.EnsureCoreWebView2Async(null);
+    }
+
+    public string FilePath 
+    { 
+        get => _filePath;
+        set 
+        {
+            if (_filePath ==value)
+            {
+                return;
+            }
+            if (string.IsNullOrEmpty( value)==true)
+            {
+                return;
+            }
+            _filePath = value;
+
+            _webView.Source = new Uri(@"file://" + _filePath);
+
+            _notifyPropertyChanged();
+        }
+    }
+    private string _filePath =string.Empty;
+
+    #region INotifyPropertyChanged
+    public event PropertyChangedEventHandler? PropertyChanged =null;
+    private void _notifyPropertyChanged([CallerMemberName]string name_="")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name_));
+    }
+    #endregion
+}
